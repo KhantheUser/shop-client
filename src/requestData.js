2598557d920d4b0b69e11081350f459f20c78a4f
Thien Duc <ducthiennguyen1601@gmail.com>
2022-12-05 21:23:32 +0700

    'second'

--- src/requestData.js
+++ src/requestData.js
@@ -3,11 +3,13 @@ import axios from 'axios'
 const BASE_URL = process.env.REACT_APP_URL_SERVER
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOGIxZWZlYjNjNmZhNjVmY2M1ODdiZiIsImlhdCI6MTY3MDE0NTQ4NSwiZXhwIjoxNjcxODczNDg1fQ.m02LqitNxtZQSwe-EQtp278efB79rFR129JUIuHiJQ8'
 export const publicRequest =  axios.create({
-    baseURL : BASE_URL
+    baseURL : BASE_URL,
+    withCredentials : false
 })
 
 export const userRequest = axios.create({
     baseURL : BASE_URL,
+    withCredentials:false,
     headers : {
         token : `Bearer ${TOKEN}`
     }
