2b13a7ad4528fec26c6f59d31dd0992a02692a55
Thien Duc <ducthiennguyen1601@gmail.com>
2022-12-05 21:38:11 +0700

    '55

--- src/requestData.js
+++ src/requestData.js
@@ -4,7 +4,7 @@ const BASE_URL = process.env.REACT_APP_URL_SERVER
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOGIxZWZlYjNjNmZhNjVmY2M1ODdiZiIsImlhdCI6MTY3MDE0NTQ4NSwiZXhwIjoxNjcxODczNDg1fQ.m02LqitNxtZQSwe-EQtp278efB79rFR129JUIuHiJQ8'
 export const publicRequest =  axios.create({
     baseURL : BASE_URL,
-    withCredentials : false,
+    // withCredentials : false,
     headers: {
         "Access-Control-Allow-Origin":
           "https://wordle-react-project.netlify.app/",
@@ -32,7 +32,7 @@ export const publicRequest =  axios.create({
 
 export const userRequest = axios.create({
     baseURL : BASE_URL,
-    withCredentials:false,
+    // withCredentials:false,
     
     headers: {
         token : `Bearer ${TOKEN}`,
